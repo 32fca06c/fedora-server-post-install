3a7cf58a3b612cfff81eab5285a2f00cbbb7461c
adner <67954372+32fca06c@users.noreply.github.com>
2022-06-25 16:08:14 +0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,19 +1,18 @@
-sudo systemctl enable --now cockpit.socket
+echo fastestmirror=True>>/etc/dnf/dnf.conf
+echo max_parallel_downloads=20>>/etc/dnf/dnf.conf
 
-sudo firewall-cmd --permanent --add-port=8080/tcp
-
-echo [Unit]>>/etc/systemd/system/qbittorrent.service
-Description=qBittorrent-nox service>/etc/systemd/system/qbittorrent.service
-Wants=network-online.target>/etc/systemd/system/qbittorrent.service
-After=network-online.target nss-lookup.target>/etc/systemd/system/qbittorrent.service
-
-[Service]>/etc/systemd/system/qbittorrent.service
-Type=exec>/etc/systemd/system/qbittorrent.service
-Restart=always>/etc/systemd/system/qbittorrent.service
-RestartSec=1>/etc/systemd/system/qbittorrent.service
-User=adner>/etc/systemd/system/qbittorrent.service
-UMask=0000>/etc/systemd/system/qbittorrent.service
-ExecStart=/usr/bin/qbittorrent-nox>/etc/systemd/system/qbittorrent.service
-
-[Install]>/etc/systemd/system/qbittorrent.service
+echo [Unit]>/etc/systemd/system/qbittorrent.service
+Description=qBittorrent-nox service>>/etc/systemd/system/qbittorrent.service
+Wants=network-online.target>>/etc/systemd/system/qbittorrent.service
+After=network-online.target nss-lookup.target>>/etc/systemd/system/qbittorrent.service
+echo.>>/etc/systemd/system/qbittorrent.service
+[Service]>>/etc/systemd/system/qbittorrent.service
+Type=exec>>/etc/systemd/system/qbittorrent.service
+Restart=always>>/etc/systemd/system/qbittorrent.service
+RestartSec=1>>/etc/systemd/system/qbittorrent.service
+User=adner>>/etc/systemd/system/qbittorrent.service
+UMask=0000>>/etc/systemd/system/qbittorrent.service
+ExecStart=/usr/bin/qbittorrent-nox>>/etc/systemd/system/qbittorrent.service
+echo.>>/etc/systemd/system/qbittorrent.service
+[Install]>>/etc/systemd/system/qbittorrent.service
 WantedBy=multi-user.target>/etc/systemd/system/qbittorrent.service
