702279f6672411a925dc18c688fa4d07f3db084d
adner <67954372+32fca06c@users.noreply.github.com>
2022-06-25 16:16:10 +0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,17 +2,17 @@ echo fastestmirror=True>>/etc/dnf/dnf.conf
 echo max_parallel_downloads=20>>/etc/dnf/dnf.conf
 
 echo [Unit]>/etc/systemd/system/qbittorrent.service
-Description=qBittorrent-nox service>>/etc/systemd/system/qbittorrent.service
-Wants=network-online.target>>/etc/systemd/system/qbittorrent.service
-After=network-online.target nss-lookup.target>>/etc/systemd/system/qbittorrent.service
-echo.>>/etc/systemd/system/qbittorrent.service
-[Service]>>/etc/systemd/system/qbittorrent.service
-Type=exec>>/etc/systemd/system/qbittorrent.service
-Restart=always>>/etc/systemd/system/qbittorrent.service
-RestartSec=1>>/etc/systemd/system/qbittorrent.service
-User=adner>>/etc/systemd/system/qbittorrent.service
-UMask=0000>>/etc/systemd/system/qbittorrent.service
-ExecStart=/usr/bin/qbittorrent-nox>>/etc/systemd/system/qbittorrent.service
-echo.>>/etc/systemd/system/qbittorrent.service
-[Install]>>/etc/systemd/system/qbittorrent.service
-WantedBy=multi-user.target>>/etc/systemd/system/qbittorrent.service
+echo Description=qBittorrent-nox service>>/etc/systemd/system/qbittorrent.service
+echo Wants=network-online.target>>/etc/systemd/system/qbittorrent.service
+echo After=network-online.target nss-lookup.target>>/etc/systemd/system/qbittorrent.service
+echo >>/etc/systemd/system/qbittorrent.service
+echo [Service]>>/etc/systemd/system/qbittorrent.service
+echo Type=exec>>/etc/systemd/system/qbittorrent.service
+echo Restart=always>>/etc/systemd/system/qbittorrent.service
+echo RestartSec=1>>/etc/systemd/system/qbittorrent.service
+echo User=adner>>/etc/systemd/system/qbittorrent.service
+echo UMask=0000>>/etc/systemd/system/qbittorrent.service
+echo ExecStart=/usr/bin/qbittorrent-nox>>/etc/systemd/system/qbittorrent.service
+echo >>/etc/systemd/system/qbittorrent.service
+echo [Install]>>/etc/systemd/system/qbittorrent.service
+echo WantedBy=multi-user.target>>/etc/systemd/system/qbittorrent.service
