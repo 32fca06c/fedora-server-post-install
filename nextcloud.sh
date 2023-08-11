af9b5bc88463313182a375dd4b048f2e94499e70
adner <67954372+32fca06c@users.noreply.github.com>
2023-08-11 22:52:03 +0500

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+# Variables
+DB_USERNAME=user
+DB_PASSWORD=password
+# Nextcloud
 sudo dnf install nextcloud-nginx -y
 sudo touch /usr/share/nextcloud/config/CAN_INSTALL
-
+# Nextcloud Storage
 sudo mkdir /home/nextcloud
 sudo chown -R nginx:nginx /home/nextcloud
 sudo semanage fcontext -a -t httpd_sys_rw_content_t '/home/nextcloud(/.*)?'
 sudo restorecon -v '/home/nextcloud'
+# Nextcloud Database
+sudo dnf install mariadb-server -y
+sudo systemctl enable --now mariadb
+sudo mysql_secure_installation
+sudo mysql -uroot -proot -e "CREATE DATABASE nextcloud;"
+sudo mysql -uroot -proot -e "CREATE USER '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+sudo mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON nextcloud.* TO '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+sudo mysql -uroot -proot -e "FLUSH PRIVILEGES;"
+# Nextcloud Cache
+sudo dnf install redis -y
+sudo systemctl enable --now redis.service
