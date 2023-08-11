5122b494bac3236085a07ee629389a241f146c2d
adner <67954372+32fca06c@users.noreply.github.com>
2023-08-11 22:28:20 +0500

    Update qbittorrent-nox.sh

--- qbittorrent-nox.sh
+++ qbittorrent-nox.sh
@@ -1,4 +1,21 @@
 #/bin/bash
 sudo dnf install qbittorrent-nox -y
+sudo tee -a /etc/systemd/system/qbittorrent.service <<EOF
+[Unit]
+Description=qBittorrent-nox service
+Wants=network-online.target
+After=network-online.target nss-lookup.target
 
+[Service]
+Type=exec
+Restart=always
+RestartSec=1
+User=$(whoami)
+UMask=0000
+ExecStart=/usr/bin/qbittorrent-nox
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
 sudo systemctl enable --now qbittorrent
