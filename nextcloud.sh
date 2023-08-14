78395e38916a68e880a160410668ddf59ad0f098
adner <67954372+32fca06c@users.noreply.github.com>
2023-08-14 14:23:38 +0500

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -8,6 +8,7 @@ sudo sed -i -e 's,user = apache,user = nginx,g' -e 's,group = apache,group = ngi
 sudo touch /usr/share/nextcloud/config/CAN_INSTALL
 sudo setfacl -R -m u:nginx:rwx /usr/share/nextcloud/*
 sudo chown -R nginx:nginx /usr/share/nextcloud/
+sudo chown nginx:nginx /usr/share/nextcloud/config/config.php
 sudo semanage fcontext -a -t httpd_sys_rw_content_t '/usr/share/nextcloud/config(/.*)?'
 sudo semanage fcontext -a -t httpd_sys_rw_content_t '/usr/share/nextcloud/apps(/.*)?'
 sudo semanage fcontext -a -t httpd_sys_rw_content_t '/usr/share/nextcloud/3rdparty/aws/aws-sdk-php/src/data/logs(/.*)?'
@@ -27,5 +28,6 @@ sudo dnf install redis -y
 sudo systemctl enable --now redis.service
 # Nextcloud Fixes
 sudo sed -i 's,HP_VERSION_ID >= 80200,HP_VERSION_ID >= 80300,g' /usr/share/nextcloud/lib/versioncheck.php
+sudo sed -i 's,'writable' => false,'writable' => true,g'/usr/share/nextcloud/config/config.php
 # Nextcloud Apps Manage
 sudo -u nginx php occ app:disable
