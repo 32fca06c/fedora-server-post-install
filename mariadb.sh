fe18786c49d85e2eb2d8301db75d7f1fc93ef094
adner <67954372+32fca06c@users.noreply.github.com>
2023-08-14 13:40:55 +0500

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 sudo dnf install mariadb-server -y
 sudo systemctl enable --now mariadb
-sudo mysql_secure_installation
+sudo mysql -uroot -proot -e "UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';"
+sudo mysql -uroot -proot -e "DELETE FROM mysql.user WHERE User='';"
+sudo mysql -uroot -proot -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+sudo mysql -uroot -proot -e "DROP DATABASE IF EXISTS test;"
+sudo mysql -uroot -proot -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+sudo mysql -uroot -proot -e "FLUSH PRIVILEGES;"
