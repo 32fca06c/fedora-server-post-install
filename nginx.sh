f2c5fa094540d56794e4d9ecf81bca3425e3bb96
adner <67954372+32fca06c@users.noreply.github.com>
2023-08-14 12:51:48 +0500

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -5,3 +5,4 @@ sudo dnf install nginx php-fpm -y
 sudo sed -i -e 's,user = apache,user = nginx,g' -e 's,group = apache,group = nginx,g' /etc/php-fpm.d/www.conf
 # Ending
 sudo systemctl enable --now php-fpm.service
+sudo systemctl enable --now nginx.service
