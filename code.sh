3cd947419dde68ffe74975fec5b1a0503de77935
adner <67954372+32fca06c@users.noreply.github.com>
2023-08-18 17:11:14 +0500

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+wget https://www.collaboraoffice.com/repos/CollaboraOnline/CODE-centos7/repodata/repomd.xml.key && sudo rpm --import repomd.xml.key
 sudo dnf config-manager --add-repo https://www.collaboraoffice.com/repos/CollaboraOnline/CODE-rpm
 sudo dnf install coolwsd CODE-brand -y
