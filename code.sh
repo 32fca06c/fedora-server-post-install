0e4ca80d0871b475508e89ee5de3aa3e141264b3
adner <67954372+32fca06c@users.noreply.github.com>
2023-08-18 17:19:22 +0500

    Update code.sh

--- code.sh
+++ code.sh
@@ -2,4 +2,8 @@
 wget https://www.collaboraoffice.com/repos/CollaboraOnline/CODE-centos7/repodata/repomd.xml.key && sudo rpm --import repomd.xml.key
 sudo dnf config-manager --add-repo https://www.collaboraoffice.com/repos/CollaboraOnline/CODE-rpm
 sudo dnf install coolwsd CODE-brand -y
+sudo coolconfig set ssl.enable false
+sudo coolconfig set ssl.termination true
+sudo coolconfig set storage.wopi.host nextcloud.example.com
+sudo coolconfig set-admin-password
 sudo systemctl enable --now coolwsd
