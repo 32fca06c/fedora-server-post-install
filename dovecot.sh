e7a58f8ef63ccff7aec0150882a08e62f75ead9f
adner <67954372+32fca06c@users.noreply.github.com>
2023-08-19 21:30:26 +0500

    Update dovecot.sh

--- dovecot.sh
+++ dovecot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo dnf install dovecot -y
-# sudo nano /etc/dovecot/config
-# sudo nano 10-ssh
+sudo nano /etc/dovecot/dovecot.conf 
+sudo nano /etc/dovecot/conf.d/10-ssl.conf 
 sudo useradd -r -m vmail
