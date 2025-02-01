4ba8a32af2b93f568fd647b03ed9b6b4b6f748cd
adner <67954372+32fca06c@users.noreply.github.com>
2025-02-01 18:50:49 +0500

    Update fedora-server.sh

--- fedora-server.sh
+++ fedora-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SCRIPT=$(realpath "$0")
-
 function first_run() {
     if [ "${EUID}" -eq 0 ]; then
         exit 1
@@ -32,26 +30,59 @@ function second_run() {
     sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
     sudo dnf update -y
     ###
+    if ! dnf list installed "samba" &> /dev/null; then
+        sudo dnf install samba -y
+        sudo systemctl enable --now smb
+        sudo firewall-cmd --permanent --add-service=samba
+        sudo firewall-cmd --reload
+        sudo smbpasswd -a $(whoami)
+    fi
+    ###
     if ! dnf list installed "cockpit" &> /dev/null; then
-        sudo dnf install cockpit cockpit-pcp -y
+        sudo dnf install cockpit pcp python3-pcp -y
         sudo systemctl enable --now cockpit.socket
+        sudo systemctl enable --now pmlogger.service
         sudo firewall-cmd --add-service=cockpit --permanent
         sudo firewall-cmd --reload
     fi
     ###
     if ! dnf list installed "jellyfin-server" &> /dev/null; then
         sudo dnf install jellyfin-server jellyfin-web intel-media-driver -y
-        sudo usermod -aG adner jellyfin
+        sudo usermod -aG $(whoami) jellyfin
         sudo systemctl enable --now jellyfin.service
     fi
     ###
     if ! dnf list installed "qbittorrent-nox" &> /dev/null; then
         sudo useradd -r -s /usr/sbin/nologin -m qbittorrent
-        sudo usermod -aG adner qbittorrent
+        sudo usermod -aG $(whoami) qbittorrent
         sudo dnf install qbittorrent-nox -y
         sudo -u qbittorrent qbittorrent-nox
     fi
-
+    ###
+    if ! dnf list installed "sing-box-beta" &> /dev/null; then
+        sudo dnf config-manager addrepo --from-repofile=https://sing-box.app/sing-box.repo
+        sudo dnf install sing-box-beta -y
+    fi
+    ###
+    if ! dnf list installed "nginx" &> /dev/null; then
+        sudo dnf install nginx -y
+        sudo usermod -aG $(whoami) nginx
+        sudo firewall-cmd --add-service=http --permanent
+        sudo firewall-cmd --add-service=https --permanent
+        sudo firewall-cmd --reload
+        sudo systemctl enable --now nginx.service
+    fi
+    ###
+    if ! dnf list installed "nodejs" &> /dev/null; then
+        sudo dnf install nodejs nodemon -y
+    fi
+    ###
+    if ! dnf list installed "powershell" &> /dev/null; then
+        sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc -y
+        curl https://packages.microsoft.com/config/rhel/9/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+        sudo dnf makecache
+        sudo dnf install powershell -y
+    fi
 }
 
 ${1:-first_run}
